7518f959b3f5f4d6f8411a26e0ab2dcc8b7b9587
EpicOrange <danieldoesntcheckthis@gmail.com>
2014-07-20 16:57:01 -0700

    Fixed init bugs

--- init.js
+++ init.js
@@ -5,6 +5,16 @@ var canvas;
 var fore, back;
 
 function init() {
+  // make buttons work
+  document.getElementById("write").addEventListener('click', function() {writeCanvas(input.value);}, false);
+  document.getElementById("parse").addEventListener('click', function() {parse(input.value);}, false);
+  document.getElementById("assemble").addEventListener('click', function() {assemble(input.value);}, false);
+  document.getElementById("undo").addEventListener('click', undo, false);
+  document.getElementById("reset").addEventListener('click', boot, false);
+  document.getElementById("run").addEventListener('click', run, false);
+  document.getElementById("step").addEventListener('click', step, false);
+
+  // setup canvas  
   viewX = document.getElementById("canvas").width / charX; // number of tiles X
   viewY = document.getElementById("canvas").height / charY; // and Y
   canvas = document.getElementById("canvas").getContext('2d'); // setup context
@@ -14,8 +24,7 @@ function init() {
   canvas.fillStyle = "white";
   fore = colors[15];
   back = colors[0];
-  input.value = "0x02148040"; // MOV 0x14 %r2
-  //input.value = "ADD 0x14 %r11 %r0"; // 0x20148084
-  //input.value = "1111 1111 0000 0000 0000 0000 0000 0000";
+
+  input.value = "MOV 0x3200 %r0 ; assemble me and run!";
   boot();
 }
